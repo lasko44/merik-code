d20ef83cc1758a4275f009af2f3d0aad465791f6
Matthew Laskiewicz <matthew.laskiewicz@milkcan.io>
2024-12-27 11:28:22 -0600

    feat(test add): adds tests and moves to semantic html

--- tests/js/Pages/ComponentLibrary/Create.test.js
+++ tests/js/Pages/ComponentLibrary/Create.test.js
@@ -1,2 +1,17 @@
 import { describe, it, expect } from "vitest";
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
+import Create from "@/Pages/ComponentLibrary/Create.vue";
+import {Head} from "@inertiajs/vue3";
+import MainLayout from "@/Layouts/MainLayout.vue";
+
+describe("Create", () => {
+    it("renders Head Component", () => {
+        const wrapper = shallowMount(Create);
+        expect(wrapper.findComponent(Head).exists()).toBe(true);
+    });
+
+    it("renders MainLayout Component", function () {
+        const wrapper = shallowMount(Create);
+        expect(wrapper.findComponent(MainLayout).exists()).toBe(true);
+    });
+});
