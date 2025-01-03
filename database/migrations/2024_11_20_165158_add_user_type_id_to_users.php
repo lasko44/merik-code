d2e953dadfc6842ce6f8c64601b7a7ba3ebd3cdd
Matthew Laskiewicz <matthew.laskiewicz@milkcan.io>
2025-01-03 11:35:36 -0600

    feat(component register): starting login for admin and submit button fix

--- database/migrations/2024_11_20_165158_add_user_type_id_to_users.php
+++ database/migrations/2024_11_20_165158_add_user_type_id_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('user_type_id')->default(1);
+            $table->foreignId('user_type_id')->default(1)->after('id');
         });
     }
 
