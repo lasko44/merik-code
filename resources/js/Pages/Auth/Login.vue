b84b60280ffc1452c0911dd26c6b3df6f5446c1c
Matthew Laskiewicz <matthew.laskiewicz@milkcan.io>
2025-01-03 12:43:35 -0600

    feat(login-component): added checkbox for login component

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -6,10 +6,12 @@ import {ref} from "vue";
 import Text from "@/Shared/Inputs/Text.vue";
 import Button from "@/Shared/Inputs/Button.vue";
 import Password from "@/Shared/Inputs/Password.vue";
+import Checkbox from "@/Shared/Inputs/Checkbox.vue";
 
 const form = useForm({
   username: ref(null),
   password: ref(null),
+  rememberMe: ref(false),
 });
 </script>
 
@@ -19,12 +21,13 @@ const form = useForm({
     <meta name="description" content="Login to Merik Code">
   </Head>
   <MainLayout>
-    <div class="flex justify-center mt-10">
+    <div class="flex justify-center mt-20">
       <StandardCard class="w-1/2" :title="'Login'">
         <section class="flex justify-center mt-6">
           <div class="w-3/4">
-            <Text label="Username"/>
-            <Password class="mt-3" label="Password"/>
+            <Text label="Username" v-model="form.username"/>
+            <Password class="mt-3" label="Password" v-model="form.password"/>
+            <Checkbox class="mt-3" label="Remember Me" v-model="form.rememberMe"/>
             <Button :disabled="false" class="w-full mt-10" text="Login"/>
           </div>
         </section>
