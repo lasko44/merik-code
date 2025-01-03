d4731150b92a0922823ecb9fabd7fae15c590e01
Matthew Laskiewicz <matthew.laskiewicz@milkcan.io>
2025-01-03 12:43:35 -0600

    feat(login-component): added checkbox for login component

---
+++ resources/js/Shared/Inputs/Checkbox.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import {defaultFalseBoolProp, requiredStringProp} from "@/Shared/Props/common.js";
+import Label from "@/Shared/Inputs/Label.vue";
+import {ref} from "vue";
+
+const props = defineProps({
+  label: requiredStringProp,
+  value: defaultFalseBoolProp,
+  required: defaultFalseBoolProp,
+});
+
+const emit = defineEmits(['update:modelValue']);
+const checked = ref(props.value);
+function check(){
+  checked.value = !checked.value;
+  emit('update:modelValue', checked.value);
+}
+
+</script>
+
+<template>
+  <div class="flex row">
+    <input class="mt-[5px] rounded focus:ring-0 text-cyan-700" type="checkbox" @input="check">
+    <Label class="ml-2" :label="label"/>
+  </div>
+
+</template>
+
