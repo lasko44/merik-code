98034c0c06a7470594b85ef31e45c93143eb990e
Matthew Laskiewicz <matthew.laskiewicz@milkcan.io>
2025-01-03 12:00:33 -0600

    feat(component-form): starts standard card for login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -3,6 +3,9 @@ import MainLayout from "@/Layouts/MainLayout.vue";
 import StandardCard from "@/Shared/Cards/StandardCard.vue";
 import {Head, useForm} from "@inertiajs/vue3";
 import {ref} from "vue";
+import Text from "@/Shared/Inputs/Text.vue";
+import Button from "@/Shared/Inputs/Button.vue";
+import Password from "@/Shared/Inputs/Password.vue";
 
 const form = useForm({
   username: ref(null),
@@ -18,7 +21,13 @@ const form = useForm({
   <MainLayout>
     <div class="flex justify-center mt-10">
       <StandardCard class="w-1/2" :title="'Login'">
-
+        <section class="flex justify-center mt-6">
+          <div class="w-3/4">
+            <Text label="Username"/>
+            <Password class="mt-3" label="Password"/>
+            <Button :disabled="false" class="w-full mt-10" text="Login"/>
+          </div>
+        </section>
       </StandardCard>
     </div>
   </MainLayout>
---
+++ resources/js/Shared/Inputs/Password.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref, watch } from 'vue';
+import {defaultFalseBoolProp, optionalStringDefaultProp, optionalStringProp} from '@/Shared/Props/common.js';
+import {COLORS} from "@/Shared/Typography/utils/classes.js";
+import Label from "@/Shared/Inputs/Label.vue";
+
+const props = defineProps({
+  value: optionalStringProp,
+  label: optionalStringProp,
+  error: defaultFalseBoolProp,
+  errorMessage: optionalStringDefaultProp("Something is Wrong")
+});
+
+const inputClass = ref('rounded mt-2 p-2 w-full border border-neutral-800 focus:outline-none focus:ring-0 focus:border-cyan-600 focus:border-2');
+
+watch(() => props.error, (newValue) => {
+  if (newValue) {
+    inputClass.value = 'rounded mt-2 p-2 w-full border border-red-600 focus:outline-none focus:ring-0 focus:border-red-600 focus:border-2';
+  } else {
+    inputClass.value = 'rounded mt-2 p-2 w-full border border-neutral-800 focus:outline-none focus:ring-0 focus:border-cyan-600 focus:border-2';
+  }
+});
+
+const emit = defineEmits(['update:modelValue']);
+</script>
+
+
+<template>
+  <div>
+    <Label :label="label"/>
+    <div>
+      <input
+          @input="$emit('update:modelValue', $event.target.value)"
+          :value="value"
+          :class="inputClass"
+          type="password"
+      >
+    </div>
+    <div v-if="error">
+      <p :class="COLORS.RED">{{errorMessage}}</p>
+    </div>
+  </div>
+
+</template>
