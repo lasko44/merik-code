4f8b4442a6690dd433a47eeac656566211dbd8ed
Matthew Laskiewicz <matthew.laskiewicz@milkcan.io>
2025-01-03 19:06:50 -0600

    feat(policy create component): register custom error handling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\Component;
+use App\Policies\ComponentPolicy;
 use App\Utilities\ComponentUtil;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Register any application services.
      */
@@ -22,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::policy(Component::class, ComponentPolicy::class);
     }
 }
